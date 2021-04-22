bb60380213129efddf23c0c65fe75177e7a92b29
wangjunxintjwuz <quanliao4838808@163.com>
2021-04-22 17:28:43 +0800

    displaying some data

--- BoringMapApp/src/main/java/pl/mg6/boringmapapp/MainActivity.java
+++ BoringMapApp/src/main/java/pl/mg6/boringmapapp/MainActivity.java
@@ -5,9 +5,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends ActionBarActivity {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,24 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new SupportMapFragment())
                     .commit();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (map == null) {
+            SupportMapFragment f = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.container);
+            map = f.getMap();
+            if (map != null) {
+                setupMap();
+            }
+        }
+    }
+
+    private void setupMap() {
+        MarkerOptions options = new MarkerOptions();
+        map.addMarker(options.title("Gdańsk").position(new LatLng(54.360, 18.639)));
+        map.addMarker(options.title("Gdynia").position(new LatLng(54.520, 18.530)));
+        map.addMarker(options.title("Sopot").position(new LatLng(54.439, 18.559)));
     }
 }
