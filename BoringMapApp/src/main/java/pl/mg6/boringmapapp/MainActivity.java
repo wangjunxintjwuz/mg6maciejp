5efc3188411f1dcaf6c5439353f5385f4bb088d8
wangjunxintjwuz <quanliao4838808@163.com>
2021-04-28 00:00:03 +0800

    added Android Maps Utils

--- BoringMapApp/src/main/java/pl/mg6/boringmapapp/MainActivity.java
+++ BoringMapApp/src/main/java/pl/mg6/boringmapapp/MainActivity.java
@@ -1,13 +1,22 @@
 package pl.mg6.boringmapapp;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import com.androidmapsextensions.ClusterOptions;
+import com.androidmapsextensions.ClusterOptionsProvider;
 import com.androidmapsextensions.ClusteringSettings;
 import com.androidmapsextensions.GoogleMap;
+import com.androidmapsextensions.Marker;
 import com.androidmapsextensions.MarkerOptions;
 import com.androidmapsextensions.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.ui.IconGenerator;
+
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -38,7 +47,20 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void setupMap() {
-        map.setClustering(new ClusteringSettings());
+        ClusteringSettings settings = new ClusteringSettings();
+        settings.clusterOptionsProvider(new ClusterOptionsProvider() {
+            @Override
+            public ClusterOptions getClusterOptions(List<Marker> markers) {
+                String text = "Trójmiasto";
+                if (markers.size() == 2) {
+                    text = "Dwumiasto";
+                }
+                Bitmap bmp = new IconGenerator(MainActivity.this).makeIcon(text);
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(bmp);
+                return new ClusterOptions().icon(icon);
+            }
+        });
+        map.setClustering(settings);
         MarkerOptions options = new MarkerOptions();
         map.addMarker(options.title("Gdańsk").position(new LatLng(54.360, 18.639)));
         map.addMarker(options.title("Gdynia").position(new LatLng(54.520, 18.530)));
